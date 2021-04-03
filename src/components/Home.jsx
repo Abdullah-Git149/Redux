5c6f437bfd20615905600ed0ea70e3e20f96aff1
Abdullah-Git149 <keepcool200@gmail.com>
2021-04-03 21:18:41 +0500

    second Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Module</th>
+                        <th scope="col">Name</th>
                         <th scope="col">Read</th>
                         <th scope="col">Write</th>
                         <th scope="col">Create</th>
